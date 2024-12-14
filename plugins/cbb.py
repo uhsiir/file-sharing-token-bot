324d2d1fbcebfa3f31887fc984ef43248be084de
uhsiir <uhsiiir@gmail.com>
2024-12-14 23:41:31 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -3,14 +3,14 @@
 from pyrogram import __version__
 from bot import Bot
 from config import OWNER_ID
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://modijiurl.com/l3GyyF'>Click here</a>\n○ 
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'></a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://modijiurl.com/l3GyyF'>Click here</a>\n○ Channel : \n○ Support Group : </b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
