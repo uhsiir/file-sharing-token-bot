e1a0e7594aa6624adb8d23d1d1e66b676a83391b
uhsiir <uhsiiir@gmail.com>
2024-12-15 00:01:17 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'></a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://modijiurl.com/l3GyyF'>Click here</a>\n○ Channel : \n○ Support Group : </b>",
+            text = text = f"<b>○ Language : <code>Python3</code>\n○ Source Code : <a href='https://modijiurl.com/l3GyyF'>Click here</a></b>"
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
